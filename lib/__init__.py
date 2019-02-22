1646a852118deba935472c08da83b22d27dc7970
Mantas Mikulėnas <grawity@gmail.com>
2019-02-22 20:02:15 +0200

    reduce verbosity

--- lib/__init__.py
+++ lib/__init__.py
@@ -20,24 +20,22 @@ def main():
     for event in enum_log_entries():
         if not quiet:
             show_log_entry(event)
+
         idx = event["pcr_idx"]
         this_extend_value = event["pcr_extend_value"]
-        next_extend_value = event["pcr_extend_value"]
+        next_extend_value = this_extend_value
 
         if event["event_type"] == TpmEventType.EFI_BOOT_SERVICES_APPLICATION:
-            ed = parse_efi_bsa_event(event["event_data"])
-            unix_path = device_path_to_unix_path(ed["device_path_vec"])
+            event_data = parse_efi_bsa_event(event["event_data"])
+            unix_path = device_path_to_unix_path(event_data["device_path_vec"])
             file_hash = hash_pecoff(unix_path, "sha1")
             next_extend_value = file_hash
-            pcr_value = this_pcrs[idx]
-            pcr_value = hashlib.sha1(pcr_value + next_extend_value).digest()
             if not quiet:
                 print("-- extending with coff hash --")
                 print("file path =", unix_path)
                 print("file hash =", to_hex(file_hash))
                 print("this event extend value =", to_hex(this_extend_value))
                 print("guessed extend value =", to_hex(next_extend_value))
-                print("guessed next pcr value =", to_hex(pcr_value))
 
         this_pcrs[idx] = hashlib.sha1(this_pcrs[idx] + this_extend_value).digest()
         next_pcrs[idx] = hashlib.sha1(next_pcrs[idx] + next_extend_value).digest()
