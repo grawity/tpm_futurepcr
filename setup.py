e08cdaaaa701dc1af4d903effa0029798f150fb6
Mantas Mikulėnas <grawity@gmail.com>
2019-02-22 19:55:57 +0200

    attempt to organize the way normal people do it

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tpm_reseal',
+      version='0.1',
+      description='',
+      url='https://github.com/grawity/tpm_futurepcr',
+      author='Mantas Mikulėnas',
+      author_email='grawity@gmail.com',
+      license='MIT',
+      packages=['lib'],
+      scripts=['tpm_futurepcr'])
