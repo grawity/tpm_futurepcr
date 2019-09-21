059178c72e7e684727d5b025b1d0ade03adb2891
Mantas Mikulėnas <grawity@gmail.com>
2019-09-21 14:40:21 +0300

    revert to having metadata in setup.py
    If a standard format like TOML or even XML will be used in the future,
    that's fine. But configparser's oddball nonspec'd variant of .ini files
    is not what I want to use (or be anywhere near).

--- setup.py
+++ setup.py
@@ -1,2 +1,14 @@
 from setuptools import setup
-setup()
+setup(name="tpm_futurepcr",
+      version="0.1",
+      description="Calculate future TPM PCR[4] after a kernel upgrade",
+      url="https://github.com/grawity/tpm_futurepcr",
+      author="Mantas Mikulėnas",
+      author_email="grawity@gmail.com",
+      license="MIT",
+      packages=["tpm_futurepcr"],
+      entry_points={
+          "console_scripts": [
+              "tpm_futurepcr = tpm_futurepcr:main",
+          ],
+      })
