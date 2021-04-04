daad17d721a50ec316d7be846654adab7f02fcfc
Mantas Mikulėnas <grawity@gmail.com>
2021-04-04 03:28:57 +0300

    pcr: parametrize the digest algo in init_empty_pcrs() and read_current_pcrs()

--- tpm_futurepcr/pcr_bank.py
+++ tpm_futurepcr/pcr_bank.py
@@ -6,12 +6,14 @@ from .util import is_tpm2, in_path
 NUM_PCRS = 24
 PCR_SIZE = hashlib.sha1().digest_size
 
-def init_empty_pcrs():
-    pcrs = {idx: (b"\xFF" if (17 <= idx <= 22) else b"\x00") * PCR_SIZE
+def init_empty_pcrs(alg="sha1"):
+    pcr_size = hashlib.new(alg).digest_size
+    pcrs = {idx: (b"\xFF" if (17 <= idx <= 22) else b"\x00") * pcr_size
             for idx in range(NUM_PCRS)}
     return pcrs
 
-def read_current_pcrs():
+def read_current_pcrs(alg="sha1"):
+    pcr_size = hashlib.new(alg).digest_size
     if is_tpm2():
         if in_path("tpm2_pcrread"): # tpm2-utils 4.0 or later
             cmd = ["tpm2_pcrread", "sha1", "-Q", "-o", "/dev/stdout"]
@@ -23,9 +25,11 @@ def read_current_pcrs():
         res = subprocess.run(cmd, stdout=subprocess.PIPE)
         res.check_returncode()
         buf = res.stdout
-        assert(len(buf) % PCR_SIZE == 0)
-        return {idx: buf[idx*PCR_SIZE:(idx+1)*PCR_SIZE] for idx in range(len(buf) // PCR_SIZE)}
+        assert(len(buf) % pcr_size == 0)
+        return {idx: buf[idx*pcr_size:(idx+1)*pcr_size] for idx in range(len(buf) // pcr_size)}
     else:
+        if alg != "sha1":
+            raise Exception("TPM1 only supports SHA1")
         pcrs = {}
         with open("/sys/class/tpm/tpm0/pcrs", "r") as fh:
             for line in fh:
