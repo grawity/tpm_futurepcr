6c47ee91658110b8a5045b333a4d3e0ae47daf58
Mantas Mikulėnas <grawity@gmail.com>
2021-08-10 15:26:35 +0300

    remove debug logging from binary_reader
    This is a copy of a copy of a copy of a copy of a helper class I wrote
    for my OBML file format reverse-engineering project.

--- tpm_futurepcr/binary_reader.py
+++ tpm_futurepcr/binary_reader.py
@@ -1,19 +1,10 @@
-import os
 import struct
-import sys
 
 class BinaryReader():
     def __init__(self, fh):
         self.fh = fh
         self._N_size = struct.calcsize("@N")
         self._P_size = struct.calcsize("@P")
-
-    def _debug(self, typ, data):
-        if os.environ.get("DEBUG"):
-            c_on = "\033[33m" if sys.stdout.isatty() else ""
-            c_off = "\033[m" if sys.stdout.isatty() else ""
-            print(c_on, "#", typ, repr(data), c_off)
-        return data
 
     def seek(self, pos, whence=0):
         return self.fh.seek(pos, whence)
@@ -25,9 +16,9 @@ class BinaryReader():
                 raise EOFError("Hit EOF after 0/%d bytes" % length)
             else:
                 raise IOError("Hit EOF after %d/%d bytes" % (len(buf), length))
-        return self._debug("raw[%d]" % length, buf)
+        return buf
 
-    def _read_fmt(self, length, fmt, typ):
+    def _read_fmt(self, length, fmt):
         buf = self.fh.read(length)
         if len(buf) < length:
             if len(buf) == 0:
@@ -35,28 +26,28 @@ class BinaryReader():
             else:
                 raise IOError("Hit EOF after %d/%d bytes" % (len(buf), length))
         data, = struct.unpack(fmt, buf)
-        return self._debug(typ, data)
+        return data
 
     def read_u8(self):
-        return self._read_fmt(1, ">B", "byte")
+        return self._read_fmt(1, ">B")
 
     def read_u16_le(self):
-        return self._read_fmt(2, "<H", "short")
+        return self._read_fmt(2, "<H")
 
     def read_u32_le(self):
-        return self._read_fmt(4, "<L", "long")
+        return self._read_fmt(4, "<L")
 
     def read_u64_le(self):
-        return self._read_fmt(8, "<Q", "quad")
+        return self._read_fmt(8, "<Q")
 
     def read_ptr(self):
-        return self._read_fmt(self._P_size, "@P", "ptr")
+        return self._read_fmt(self._P_size, "@P")
 
     def read_size(self):
-        return self._read_fmt(self._N_size, "@N", "size")
+        return self._read_fmt(self._N_size, "@N")
 
     def read_ptr_le(self):
-        return self._read_fmt(self._P_size, "<Q" if self._P_size == 8 else "<L", "ptr")
+        return self._read_fmt(self._P_size, "<Q" if self._P_size == 8 else "<L")
 
     def read_size_le(self):
-        return self._read_fmt(self._N_size, "<Q" if self._N_size == 8 else "<L", "size")
+        return self._read_fmt(self._N_size, "<Q" if self._N_size == 8 else "<L")
