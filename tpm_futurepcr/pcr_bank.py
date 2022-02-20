1affff7e832579560b0b392196dbdb26e21e9662
Mantas Mikulėnas <grawity@gmail.com>
2022-02-20 21:26:47 +0200

    pcr_bank: deduplicate return

--- tpm_futurepcr/pcr_bank.py
+++ tpm_futurepcr/pcr_bank.py
@@ -8,14 +8,13 @@ NUM_PCRS = 24
 
 def read_current_pcrs(alg="sha1"):
     pcr_size = hashlib.new(alg).digest_size
+    pcrs = {}
     if os.path.exists("/sys/class/tpm/tpm0/pcr-%s/0" % alg):
         # New sysfs exports in kernel v5.12
-        pcrs = {}
         for idx in range(NUM_PCRS):
             with open("/sys/class/tpm/tpm0/pcr-%s/%d" % (alg, idx), "r") as fh:
                 buf = fh.read().strip()
                 pcrs[idx] = bytes.fromhex(buf)
-        return pcrs
     elif is_tpm2():
         if in_path("tpm2_pcrread"): # tpm2-utils 4.0 or later
             cmd = ["tpm2_pcrread", alg, "-Q", "-o", "/dev/stdout"]
@@ -24,24 +23,21 @@ def read_current_pcrs(alg="sha1"):
         else:
             # TODO: try using IBM TSS tools
             raise Exception("tpm2_pcrread or tpm2_pcrlist not found")
-        pcrs = {}
         with subprocess.Popen(cmd, stdout=subprocess.PIPE) as proc:
             if proc.wait() != 0:
                 raise subprocess.CalledProcessError(proc.returncode, proc.args)
             for idx in range(NUM_PCRS):
                 pcrs[idx] = proc.stdout.read(pcr_size)
-        return pcrs
     else:
         if alg != "sha1":
             raise Exception("TPM v1 only supports the SHA1 PCR bank")
-        pcrs = {}
         with open("/sys/class/tpm/tpm0/pcrs", "r") as fh:
             for line in fh:
                 if line.startswith("PCR-"):
                     idx, buf = line.strip().split(": ")
                     idx = int(idx[4:], 10)
                     pcrs[idx] = bytes.fromhex(buf)
-        return pcrs
+    return pcrs
 
 def extend_pcr_with_hash(pcr_value, extend_value, alg="sha1"):
     pcr_value = hashlib.new(alg, pcr_value + extend_value).digest()
