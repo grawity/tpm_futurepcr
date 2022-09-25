a92886553bedadc211fd581ce2e3cde1aae753a2
Mantas Mikulėnas <grawity@gmail.com>
2022-09-25 22:05:09 +0300

    release v1.5

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name="tpm_futurepcr",
-      version="1.4",
+      version="1.5",
       description="Calculate future TPM PCR[4] after a kernel upgrade",
       url="https://github.com/grawity/tpm_futurepcr",
       author="Mantas Mikulėnas",
